532d19080a6500b538cefa247ede36a534ab10a7
nithiinnm <nithiinnm@gmail.com>
2022-06-30 23:10:03 +0530

    adding TestController

---
+++ src/main/java/com/dynamicsearchreports/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.dynamicsearchreports.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TestController {
+
+	public String hello() {
+		return "HAI";
+		
+	}
+	
+}
